2d9b589f5a7c6d11c4bafaf4913df0d192d6ce1b
Igor <igor_volokitin1@mail.ru>
2015-03-13 02:41:01 +0200

    Pods4itat koli4estvo sosednix odinakovix elementov

---
+++ sosed/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /* Pods4itat' koli4estvo par sosednix elementov s odinakovimi zna4eniyami */
+    int sizer = 10;
+    int *arr = new int[sizer];
+    int counter = 0;
+
+    for(int i = 0; i < sizer; i++) {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < sizer; i++) {
+        cout << arr[i] << "  ";
+    }
+
+    for(int i = 0; i < sizer; i++) {
+        if(arr[i] == arr[i+1]) counter++;
+    }
+
+    cout << endl << "Sosedi elementi povtoryautsa " << counter << " raz(a)" << endl;
+
+    delete [] arr;
+    return 0;
+}

44db33e4b8454996cb81b755536e2082b32ce89f
Igor <igor_volokitin1@mail.ru>
2015-03-17 16:52:41 +0200

    automat game

--- automat/main.cpp
+++ automat/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 
@@ -6,24 +5,26 @@ using namespace std;
 
 static int a;
 static int balance = 5;
-static int arrayofNumbers[] = {153,146,121,167,117,151,185,170,191,194,242,240,111,155,124,222,241,204,130,243,200,216,227,223,226,203,277,211,356,333,336,341,337,382,331,372,377,392,230,401,442,448,447,423,428,402,429,477,444,422,511,577,524,528,552,523,563,515,555,525,557,502,564,653,651,604,772,707,776,777,752,788,714,701,702,703,723,770,449,425,482,101,102,103,104,105,106,107,108,109,110,202,203,204,205,206,207,208,209,210};
+static int arrayofNumbers[] = {153,146,121,167,117,176,151,185,170,191,194,242,240,111,155,124,222,241,204,130,243,200,216,227,223,226,203,277,211,356,333,336,341,337,382,331,372,377,392,230,401,442,448,447,423,428,402,429,477,444,422,511,577,524,528,552,523,563,515,555,525,557,502,564,653,651,604,772,707,776,777,752,788,714,701,702,703,723,770,449,425,482,101,102,103,104,105,106,107,108,109,110,202,203,204,205,206,207,208,209,210};
 static bool agree = true;
-static string foragree;
+static char foragree;
 
-// -- proto of function --
 void play();
-// -- proto of function --
+
 void newgame();
+
 int main()
 {
 
     srand(time(NULL));
+
     system("color 27"); // change color of the system window
 
     newgame();
 
     cout << endl << "Your balance: " << balance << endl << endl;
-    if(balance == 0) {
+
+    if(balance == 0 || (!agree)) {
             cout << endl << endl << endl << "\t\t\t   -----The End-----" << endl << endl << endl << "\t\t\tAuthor: Igor Volokitin";
         for(int i = 0; i < 6; i++) {
             cout << endl;
@@ -32,9 +33,11 @@ int main()
     return 0;
 }
 
+/****************** CONFIGURATION START *****************/
+
 void play() {
 
-    a = arrayofNumbers[rand() % 100];
+    a = arrayofNumbers[rand() % 101];
 
 
     if(a == 111) balance += 6;
@@ -46,6 +49,10 @@ void play() {
 
 }
 
+/****************** CONFIGURATION END *****************/
+
+/****************** INFO START ******************/
+
 void newgame() {
      while(balance > 0 && agree == true) {
         cout << "Balance: " << balance << endl;
@@ -54,17 +61,25 @@ void newgame() {
         cout << "Do you want to play? (type Y if YES, D for DOUBLE game and N if NO)" << endl;
         cin >> foragree;
 
-        // IF YES
-        if(foragree == "Y" || foragree == "y") {
+        switch(foragree) {
+
+    case 'y': case 'Y':
         agree = true;
         balance--;
         play();
         system("cls");
+        break;
+
+
+    case 'n': case 'N':
+        agree = false;
+        system("cls");
+        break;
+
         }
-        // IF YES
 
         // IF DOUBLE GAME
-        if(foragree == "D" || foragree == "d" && balance >= 2) {
+        if((foragree == 'd' || foragree == 'D') && balance >= 2) {
 
             agree = true;
 
@@ -77,12 +92,7 @@ void newgame() {
         }
         // IF DOUBLE GAME
 
-        // IF NO
-       if(foragree == "N" || foragree == "n") {
-            agree = false;
-            system("cls");
-            }
-        // IF NO
     }
 
 }
+/****************** INFO END ******************/
